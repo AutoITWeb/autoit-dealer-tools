b570b4d291ad6c47db24d9003a981e915725beb5
Jesper Ager <ja@biltorvet.dk>
2019-08-23 11:42:27 +0200

    test release

--- autoit-dealer-tools.php
+++ autoit-dealer-tools.php
@@ -3,7 +3,7 @@
 Plugin Name: AutoIT Dealer Tools
 Plugin URI:  http://www.biltorvet.as/hjemmesider
 Description: Tools providing connection to AutoDesktop, and other Biltorvet services.
-Version:     2.0.5
+Version:     2.0.6
 Author:      Biltorvet A/S
 Author URI:  http://www.biltorvet.as
 License:     Proprietary
