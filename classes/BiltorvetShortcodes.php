a44eb2459847d13b7b71cb58ed75f0cac671c178
Kim Padkjær <kp@biltorvet.dk>
2019-11-19 14:24:17 +0100

    2.1.1 - hotfix

--- classes/BiltorvetShortcodes.php
+++ classes/BiltorvetShortcodes.php
@@ -344,7 +344,7 @@ if (!defined( 'ABSPATH' )) exit; // Exit if accessed directly
                 // publicName of "Price" changed to show "ink. devcost"
                 if($prop->id === 'Price')
                 {
-                    $prop->publicName = "Pris (ink. leveringsomkostninger)";
+                    $prop->publicName = "Pris (ink. lev. omkostninger)";
                 }
             }
 
