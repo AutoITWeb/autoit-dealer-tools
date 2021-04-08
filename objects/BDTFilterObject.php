7801010a3c138c91a4d39cd4a1176355c3e78827
Kim Padkjær <kp@autoit.dk>
2021-04-08 14:22:29 +0200

    2.2.5: Added full text search

--- objects/BDTFilterObject.php
+++ objects/BDTFilterObject.php
@@ -14,6 +14,7 @@
         public $ConsumptionMin;
         public $ConsumptionMax;
         public $VehicleStates;
+        public $FullTextSearch;
         public $Start;
         public $Limit;
         public $OrderBy;
