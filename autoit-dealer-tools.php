c40c2837096fd6b1797e6d28590b41edbad8ff4a
Kim Padkjær <kp@autoit.dk>
2021-06-24 12:09:39 +0200

    2.2.7 - release (hotfix)

--- autoit-dealer-tools.php
+++ autoit-dealer-tools.php
@@ -3,7 +3,7 @@
 Plugin Name: AutoIT Dealer Tools
 Plugin URI:  http://www.autoit.dk/hjemmesider
 Description: Tools providing connection to AutoDesktop, and other Biltorvet services.
-Version:     2.2.6
+Version:     2.2.7
 Author:      Auto IT A/S
 Author URI:  http://www.autoit.dk
 License:     Proprietary
--- v2/biltorvet.php
+++ v2/biltorvet.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Biltorvet
  * Plugin URI:        http://www.biltorvet.as/hjemmesider
  * Description:       Tools providing connection to AutoDesktop, and other Biltorvet services.
- * Version:           2.2.6
+ * Version:           2.2.7
  * Requires at least: 7.1
  * Requires PHP:      7.2
  * Author:            Biltorvet A/S
