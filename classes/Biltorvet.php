e2579201349e97905ca4262d7c8b515476ec089b
Kim Padkjær <53753019+kpAutoit@users.noreply.github.com>
2022-12-02 12:19:37 +0100

    3.1.6: Beautifying filters when loading. Better handling of scrolling (on search page, gettting ready for QP search)

--- classes/Biltorvet.php
+++ classes/Biltorvet.php
@@ -270,14 +270,13 @@ class Biltorvet
     {
         wp_register_script( 'bootstrap_slider', plugins_url('scripts/bootstrap-slider.min.js',  dirname(__FILE__) ) , array('jquery'), '1.0.1', true );
 
-        //wp_register_script( 'bdt_script', plugins_url('v3/scripts/vehiclesearchv3.min.js',  dirname(__FILE__) ) , array('jquery', 'bootstrap_slider'), '1.0.1', true );
         //wp_register_script( 'bdt_script', plugins_url('v3/scripts/vehiclesearchmultiselect.js',  dirname(__FILE__) ) , array('jquery', 'bootstrap_slider'), '1.0.1', true );
         wp_register_script( 'bdt_script', plugins_url('v3/scripts/vehiclesearchmultiselect.min.js',  dirname(__FILE__) ) , array('jquery', 'bootstrap_slider'), '1.0.1', true );
 
         wp_register_script('select2', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', true);
 
         wp_register_script( 'bt_slideshow', 'https://gallery.autoit.dk/latest/gallery.js', true );
-        wp_register_script( 'search_script', plugins_url('scripts/search.js',  dirname(__FILE__) ) , array('jquery'), '1.0.0', true );
+        //wp_register_script( 'search_script', plugins_url('scripts/search.js',  dirname(__FILE__) ) , array('jquery'), '1.0.0', true );
 
         // Good old widget connector - prim. needed for our exchangepricewidget
         wp_enqueue_script( 'bdt_widgetconnector', 'https://services.autoit.dk/Embed.js', null, '1.0.0', true);
